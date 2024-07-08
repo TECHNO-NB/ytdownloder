9ade278f0f3f9a5c0c1bab4b251d26968130e182
NareshBhattarai4 <technologyandapplication@gmail.com>
2024-07-08 10:53:05 +0545

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,26 +9,23 @@ const Page: React.FC = () => {
   const [newurl, setNewUrl] = useState<string>("");
 
   const fetchVideo = async () => {
+    if(url==="") return ;
     const defaultUrl = "https://img.youtube.com/vi/";
-
-    // Extract video id from the YouTube URL
-    let videoId = url.replace("https://youtu.be/", "");
-
+ let videoId = url.replace("https://youtu.be/", "");
     videoId = videoId.split("?")[0];
-
     const newLink = `${defaultUrl}${videoId}/hqdefault.jpg`;
     setNewUrl(newLink);
-    const atag=document.createElement("a");
-    atag.href=newLink;
-    atag.download="hqdefault";
-    atag.click();
-
-    
+    const atag = document.createElement("a");
+    atag.href = newLink;
+    atag.download = "hqdefault";
+   
   };
 
   return (
-    <div className="flex flex-col items-center pt-36 text-center h-[90vh] text-white">
-      <h1 className="text-2xl sm:text-6xl">YOUTUBE VIDEO DOWNLOADER</h1>
+    <div className="flex flex-col items-center pt-20 text-center h-[90vh] w-[100vw] text-white sm:pt-36 px-1">
+      <h1 className="text-5xl sm:text-6xl">
+        <span className="text-[#FF0000]">YOUTUBE</span> VIDEO DOWNLOADER
+      </h1>
       <div className="text-1xl mt-1 sm:text-4xl">
         <Typewriter
           options={{
@@ -50,13 +47,14 @@ const Page: React.FC = () => {
         />
 
         <Button onClick={fetchVideo} variant={"destructive"}>
-          Download<MdFileDownload className="text-white text-xl bg-red-500" />
+          Download
+          <MdFileDownload className="text-white text-xl bg-red-500" />
         </Button>
       </div>
 
       {newurl && (
-        <div className="mt-4">
-          <img src={newurl} alt="thumbnail" />
+        <div className="mt-4 flex items-center justify-center  px-1 py-1">
+          <img className="w-96 h-max content-center" src={newurl} alt="thumbnail" />
         </div>
       )}
     </div>
